8ae4cc40c70fca585cf3ba93436059032d7df2df
owjk123 <160257207+owjk123@users.noreply.github.com>
2026-04-10 14:04:05 +0800

    fix: capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.inkos.app',
-  appName: 'InkOSMobile',
-  webDir: 'dist'
+  appId: 'com.flashplayer.app',
+  appName: 'FlashPlayer',
+  webDir: 'dist',
+  android: {
+    backgroundColor: '#0d0d14',
+    allowMixedContent: true,
+    captureInput: true,
+  },
 };
 
 export default config;
